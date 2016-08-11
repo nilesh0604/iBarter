b0f50044cc749f161ec20650af30478dc769a315
Nilesh Shinde <nilesh.0604@gmail.com>
2016-08-11 18:06:07 +0800

    Image slider added

--- app/scripts/controllers/product.js
+++ app/scripts/controllers/product.js
@@ -8,10 +8,23 @@
  * Controller of the iBarterApp
  */
 angular.module('iBarterApp')
-  .controller('ProductCtrl', function () {
-    this.awesomeThings = [
-      'HTML5 Boilerplate',
-      'AngularJS',
-      'Karma'
-    ];
-  });
+    .controller('ProductCtrl', function($scope) {
+        this.awesomeThings = [
+            'HTML5 Boilerplate',
+            'AngularJS',
+            'Karma'
+        ];
+
+
+        $scope.initSlider = function() {
+            $(function() {
+                // wait till load event fires so all resources are available
+                $('.flexslider').flexslider({
+                    animation: "slide",
+                    controlNav: "thumbnails"
+                });
+            });
+        };
+
+        $scope.initSlider();
+    });
